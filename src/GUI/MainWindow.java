b2d9dcf490aee30757c43e209d98af62400d8b0b
Wiktor Kettel <wiktor@kettel.se>
2011-11-26 13:14:05 +0100

    Nu kan man klicka och välja objekt. Pop-up dialog härnäst

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -5,6 +5,8 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
@@ -14,13 +16,20 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import GetWebsite.GetWebsite;
 import GetWebsite.Lens;
-
+/*
+ * TODO: Vore snyggt med laddruta/progressbar när data laddas ned
+ * TODO: Fönster med mer info när man klickar på item i listan
+ */
 public class MainWindow extends JPanel{
 	JList list;
 	DefaultListModel model;
+	GetWebsite gw;
+	List<Lens> allPages;
 
 	public MainWindow() throws Exception {
 		SwingUtilities.invokeLater(new Runnable() {
@@ -28,14 +37,8 @@ public class MainWindow extends JPanel{
 				createAndShowGUI();
 			}
 		});
-		
-		
-		
 		model = new DefaultListModel();
-		
-		
-
-		
+		allPages = null;
 	}
 
 	protected void createAndShowGUI() {
@@ -51,45 +54,77 @@ public class MainWindow extends JPanel{
 		pane.add(getButton,BorderLayout.SOUTH);
 		pane.add(scrollPane, BorderLayout.NORTH);
 		
-		getButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				Runnable runnable = new Runnable() {
-					@Override
-					public void run() {
-						// Hämta listan här
-						GetWebsite gw;
-						List<Lens> allPages = null;
-						try {
-							gw = new GetWebsite();
-							allPages = gw.getAllPages();
-						} catch (Exception e) {
-						}
-						
-						// Töm modellen så listan inte appendas varje gång knappen trycks ned
-						model.removeAllElements();
-						for (Lens lens : allPages){
-							model.addElement(lens.getLens());
-						}
-						
-						SwingUtilities.invokeLater(new Runnable() {
-							public void run() {
-								// Uppdatera listan i framen här
-								list.setListData(model.toArray());
-							}
-						});
-					}
-				};
-				new Thread(runnable).start();
-			}
-		});
+		ListenerHelper lh = new ListenerHelper();
+		
+		getButton.addActionListener(lh);
+		list.addMouseListener(lh);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
 		
 	}
+	
+	class ListenerHelper implements ActionListener, MouseListener{
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			Runnable runnable = new Runnable() {
+				@Override
+				public void run() {
+					// Hämta listan här
+					try {
+						gw = new GetWebsite();
+						allPages = gw.getAllPages();
+					} catch (Exception e) {
+					}
+					
+					// Töm modellen så listan inte appendas varje gång knappen trycks ned
+					model.removeAllElements();
+					for (Lens lens : allPages){
+						model.addElement(lens.getLens());
+					}
+					
+					SwingUtilities.invokeLater(new Runnable() {
+						public void run() {
+							// Uppdatera listan i framen här
+							list.setListData(model.toArray());
+						}
+					});
+				}
+			};
+			new Thread(runnable).start();
+		}
 
+		@Override
+		public void mouseClicked(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			System.out.println("Valt: "+list.getSelectedValue());
+			Lens selectedLens = null;
+			for(Lens lens:allPages){
+				if(lens.getLens().equals(list.getSelectedValue())){
+					selectedLens = lens;
+				}
+			}
+			System.out.println("Kostar: "+selectedLens.getPrice()+selectedLens.getMonetaryUnit());
+			System.out.println("URL: "+selectedLens.getUrl());
+		}
+	}
+	
 	public static void main(String s[]) throws Exception {
 		new MainWindow();
 	}
