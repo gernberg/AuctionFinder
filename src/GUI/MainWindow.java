d65d625fabf547fe65b0855f011206781bebea99
Wiktor Kettel <wiktor@kettel.se>
2011-11-26 14:08:20 +0100

    Exception fixad. Vetefasen varför.. Men tja fixat.

--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -3,12 +3,15 @@ package GUI;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -52,6 +55,7 @@ public class MainWindow extends JFrame{
 		panel = new JPanel();
 		pris = new JLabel();
 		url = new JLabel();
+		panel.setLayout(new GridLayout(2,0,-20,0));
 		panel.add(pris);
 		panel.add(url);
 		panel.setPreferredSize(new Dimension(500,100));
